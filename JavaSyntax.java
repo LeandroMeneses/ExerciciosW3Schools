a60c0783fdc9370831d227b42857f5cdc08905f8
Leandro Meneses <souza.meneses@gmail.com>
2022-10-13 20:26:04 -0300

    JavaSyntax

---
+++ JavaSyntax.java
@@ -0,0 +1,6 @@
+public class JavaSyntax {
+    public static void main(String[] args) {
+        String texto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("O comprimento do texto String é: " + texto.length());
+    }
+}
